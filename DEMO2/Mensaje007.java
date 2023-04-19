341e694435026b9e22bca8d674f3c5fc6e1e0f99
pablomendoza76 <116389858+pablomendoza76@users.noreply.github.com>
2023-04-19 16:18:16 -0500

    Add files via upload

---
+++ DEMO2/Mensaje007.java
@@ -0,0 +1,7 @@
+package DEMO2;
+
+public class Mensaje007 {
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+}
